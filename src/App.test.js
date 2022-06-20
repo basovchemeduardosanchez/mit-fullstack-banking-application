ca6d519ac1f9cd6b29878f2fcea2438823b14eaa
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2022-06-20 08:27:33 -0500

    chore: linting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    // eslint-disable-next-line no-undef
+    expect(linkElement).toBeInTheDocument();
 });
